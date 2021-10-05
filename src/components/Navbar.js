70186e3cab543e28c3c21c0e5dd4f1811b1af5cb
Adeel Ahsan <adeelahsan5640@gmail.com>
2021-10-05 10:37:08 -0700

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,57 @@
 import React from 'react'
+import styled, { css } from 'styled-components/macro'
+import { Link } from 'react-router-dom'
+import { menuData } from '../data/MenuData'
+
+const Nav = styled.nav`
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: black;
+`
+const Navlink = css`
+  color: white;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
+`
+
+const Logo = styled(Link)`
+  ${Navlink}
+  font-style: italic;
+  text-decoration: none;
+`
+const MenuBars = styled.i``
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const NavMenuLinks = styled(Link)`
+  ${Navlink}
+`
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <Nav>
+      <Logo to='/'>ELIXR</Logo>
+      <MenuBars />
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+    </Nav>
   )
 }
 
