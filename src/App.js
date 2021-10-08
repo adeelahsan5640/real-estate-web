4c5d344abd1d38bcf08e29a2d3ce3762c237e3e8
Adeel Ahsan <adeelahsan5640@gmail.com>
2021-10-08 10:55:39 -0700

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import GlobalStyle from './globalStyle'
 
 function App() {
-  return <Navbar />
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar />
+    </>
+  )
 }
 
 export default App
