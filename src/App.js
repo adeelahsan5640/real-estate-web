4d3aa5c0cddfd70b1420aa65c8bfbf350f9bc065
Adeel Ahsan <adeelahsan5640@gmail.com>
2021-10-11 11:30:19 -0700

    slider complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import GlobalStyle from './globalStyle'
+import { SliderData } from './data/SliderData'
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Navbar />
+      <Hero slides={SliderData} />
     </>
   )
 }
